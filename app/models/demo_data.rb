d8f27b17b177bbd567216f58a06ac6ee7b78a17f
fushang318 <fushang318@gmail.com>
2016-07-05 18:54:20 +0800

    增加学习进度汇总第二个维度数据

--- app/models/demo_data.rb
+++ app/models/demo_data.rb
@@ -2,4 +2,284 @@ class DemoData
   def self.study_progress_data
     {bottom_name:"知识点",height_name:"学习进度",category_name:"学习者",items:[{name:"与客户进行前期沟通",nums:[{category_value:"张三",num:100},{category_value:"李四",num:95},{category_value:"王五",num:10},{category_value:"赵六",num:10},{category_value:"钱七",num:0},{category_value:"孙八",num:88}]},{name:"客户维护与管理",nums:[{category_value:"张三",num:0},{category_value:"李四",num:47},{category_value:"王五",num:100},{category_value:"赵六",num:0},{category_value:"钱七",num:100},{category_value:"孙八",num:100}]},{name:"寻找目标客户",nums:[{category_value:"张三",num:100},{category_value:"李四",num:0},{category_value:"王五",num:100},{category_value:"赵六",num:79},{category_value:"钱七",num:100},{category_value:"孙八",num:0}]},{name:"掌握客户信息",nums:[{category_value:"张三",num:43},{category_value:"李四",num:100},{category_value:"王五",num:0},{category_value:"赵六",num:0},{category_value:"钱七",num:81},{category_value:"孙八",num:0}]},{name:"正式接触客户",nums:[{category_value:"张三",num:100},{category_value:"李四",num:0},{category_value:"王五",num:0},{category_value:"赵六",num:100},{category_value:"钱七",num:29},{category_value:"孙八",num:0}]},{name:"销售的促成",nums:[{category_value:"张三",num:0},{category_value:"李四",num:0},{category_value:"王五",num:100},{category_value:"赵六",num:0},{category_value:"钱七",num:56},{category_value:"孙八",num:100}]},{name:"电话沟通的礼仪",nums:[{category_value:"张三",num:78},{category_value:"李四",num:13},{category_value:"王五",num:100},{category_value:"赵六",num:100},{category_value:"钱七",num:76},{category_value:"孙八",num:25}]},{name:"网点营销的礼仪",nums:[{category_value:"张三",num:0},{category_value:"李四",num:100},{category_value:"王五",num:5},{category_value:"赵六",num:19},{category_value:"钱七",num:0},{category_value:"孙八",num:0}]},{name:"电话营销话术",nums:[{category_value:"张三",num:0},{category_value:"李四",num:91},{category_value:"王五",num:100},{category_value:"赵六",num:100},{category_value:"钱七",num:100},{category_value:"孙八",num:0}]},{name:"营销风险类型",nums:[{category_value:"张三",num:100},{category_value:"李四",num:100},{category_value:"王五",num:81},{category_value:"赵六",num:0},{category_value:"钱七",num:98},{category_value:"孙八",num:71}]}]}
   end
+
+  def self.study_progress_data_2
+    {
+      "bottom_name": "学习者",
+      "height_name": "学习进度",
+      "category_name": "知识点",
+      "items": [
+        {
+          "name": "张三",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 100
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 0
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 100
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 43
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 100
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 0
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 78
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 0
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 0
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 100
+            }
+          ]
+        },
+        {
+          "name": "李四",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 95
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 47
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 0
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 100
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 0
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 0
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 13
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 100
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 91
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 100
+            }
+          ]
+        },
+        {
+          "name": "王五",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 10
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 100
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 100
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 0
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 0
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 100
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 100
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 5
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 100
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 81
+            }
+          ]
+        },
+        {
+          "name": "赵六",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 10
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 0
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 79
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 0
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 100
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 0
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 100
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 19
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 100
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 0
+            }
+          ]
+        },
+        {
+          "name": "钱七",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 0
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 100
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 100
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 81
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 29
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 56
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 76
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 0
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 100
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 98
+            }
+          ]
+        },
+        {
+          "name": "孙八",
+          "nums": [
+            {
+              "category_value": "与客户进行前期沟通",
+              "num": 88
+            },
+            {
+              "category_value": "客户维护与管理",
+              "num": 100
+            },
+            {
+              "category_value": "寻找目标客户",
+              "num": 0
+            },
+            {
+              "category_value": "掌握客户信息",
+              "num": 0
+            },
+            {
+              "category_value": "正式接触客户",
+              "num": 0
+            },
+            {
+              "category_value": "销售的促成",
+              "num": 100
+            },
+            {
+              "category_value": "电话沟通的礼仪",
+              "num": 25
+            },
+            {
+              "category_value": "网点营销的礼仪",
+              "num": 0
+            },
+            {
+              "category_value": "电话营销话术",
+              "num": 0
+            },
+            {
+              "category_value": "营销风险类型",
+              "num": 71
+            }
+          ]
+        }
+      ]
+    }
+  end
 end
